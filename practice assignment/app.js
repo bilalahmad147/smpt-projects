af432eae60a2d249655f7b19d3dbf4bebcc6b47e
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2021-02-20 01:52:15 +0500

    updated changes

---
+++ practice assignment/app.js
@@ -0,0 +1,12 @@
+// Chapter 1
+
+// alert('Welcome Bilal Ahmad ')
+
+// alert('Error! Please enter a valid password.')
+
+// alert("Welcome to JS land " + "\n"
+//     + "Happy Coding.");
+
+// alert('Welcome to JS land..')
+// alert('Happy coding')
+
