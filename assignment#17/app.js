23e7ce45a1c0c85055477c3c11ab4c41ba7c995e
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2021-03-31 17:59:29 +0500

    Update app.js

--- assignment#17/app.js
+++ assignment#17/app.js
@@ -1,2 +1,46 @@
-alert("hello world");
-console.log("hello world");
+// Starting learning JS for Gamica Cloud
+// alert("hello world");
+
+// var name = "bilal ahmad";
+// console.log(name);
+
+// var num1 = 10;
+// var num2 = 5;
+// var plus = num1 + num2;
+// console.log(plus);
+// var nameChar = num2 + name;
+// console.log(nameChar);
+
+// var firstName = "bilal";
+// var lastName = "ahmad";
+// var fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+// var value = prompt("Enter input Value: ");
+// console.log(typeof(value));
+// var value = +prompt("Enter input Value: ");
+// console.log(typeof(value));
+
+// var num = 10;
+// var name = "bilal";
+// var inputValue = prompt("Enter name : ");
+// if (inputValue === name) {
+//   num++;
+// } else {
+//   num--;
+// }
+// console.log(num);
+
+// var namesList = ["bilal", "ali", "shah", "ans"];
+// namesList.pop();
+// namesList.push("zia", "chan");
+// console.log(namesList);
+// var newList = namesList.slice(1, 4);
+// console.log(newList);
+
+// var name = "billa";
+// for (let i = 0; i <= 5; i++) {
+//   console.log(i + name);
+// }
+
+
